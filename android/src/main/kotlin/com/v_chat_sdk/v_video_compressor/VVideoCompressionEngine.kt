3ad2f3e954853ac11e931f52e56c594a94bf8ae5
Gabianne01 <bgagaci@gmail.com>
2025-10-27 13:28:16 +0200

    bumped version to V.1.8.0

--- android/src/main/kotlin/com/v_chat_sdk/v_video_compressor/VVideoCompressionEngine.kt
+++ android/src/main/kotlin/com/v_chat_sdk/v_video_compressor/VVideoCompressionEngine.kt
@@ -24,9 +24,6 @@ import androidx.media3.transformer.ExportException
 import androidx.media3.transformer.ExportResult
 import androidx.media3.transformer.TransformationRequest
 import androidx.media3.transformer.Transformer
-import androidx.media3.transformer.Effects
-import androidx.media3.effect.Presentation
-import androidx.media3.effect.ScaleAndRotateTransformation
 import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.math.max
